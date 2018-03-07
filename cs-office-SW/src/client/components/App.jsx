41a970d4952d5a10d63860a06fee1be640005dd7
Starvon Washington <staronejazz@yahoo.com>
2018-03-07 11:19:49 -0800

    react-setup

---
+++ cs-office-SW/src/client/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import LoginPage from './LoginPage.jsx';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <LoginPage/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ cs-office-SW/src/client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.jsx';
+
+import styles from './css/main.css';
+
+render(
+  <App />,
+  document.getElementById('root')
+);
