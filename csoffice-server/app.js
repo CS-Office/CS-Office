bb9d69e80d52961bf503eea7b3bcb57af3776e0a
Starvon Washington <staronejazz@yahoo.com>
2018-03-15 13:00:42 -0700

    created post routes

--- csoffice-server/app.js
+++ csoffice-server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const router = express.Router();
 const pg = require('pg');
 const path = require('path');
 const bodyParser = require('body-parser');
+const routes = require('./routes/routes.js');
 
 // pool instance for queries to db
 const pool = require('./db');
@@ -10,25 +10,8 @@ const pool = require('./db');
 const app = express();
 const PORT = 5555;
 
-// Get Request all users
-app.get('/users', (request, response, next) => {
-  pool.query('SELECT * FROM foo ORDER by id ASC', (err, res) => {
-    // pass error to error middleware
-    if (err) return next(err);
-    response.json(res.rows);
-  });
-});
-
-// Get request grab single user
-app.get('/users/:id', (request, response, next) => {
-  const { id } = request.params;
-
-  // id = $1, represents a one index array containing the id variables we want to be inserted into the string
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (err, res ) => {
-    if (err) return next (err);
-    response.json(res.rows);
-  });
-});
+app.use(bodyParser.json());
+app.use('/users', routes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
--- csoffice-server/routes/routes.js
+++ csoffice-server/routes/routes.js
@@ -4,8 +4,8 @@ const pool = require('../db');
 const router = Router();
 
 // Get Request all users
-router.get('/users', (request, response, next) => {
-  pool.query('SELECT * FROM foo ORDER by id ASC', (err, res) => {
+router.get('/', (request, response, next) => {
+  pool.query('SELECT * FROM users ORDER by id ASC', (err, res) => {
     // pass error to error middleware
     if (err) return next(err);
     response.json(res.rows);
@@ -13,7 +13,7 @@ router.get('/users', (request, response, next) => {
 });
 
 // Get request grab single user
-router.get('/users/:id', (request, response, next) => {
+router.get('/:id', (request, response, next) => {
   const { id } = request.params;
 
   // id = $1, represents a one index array containing the id variables we want to be inserted into the string
@@ -21,6 +21,19 @@ router.get('/users/:id', (request, response, next) => {
     if (err) return next (err);
     response.json(res.rows);
   });
+});
+
+// Create Post request - grab attributes from schema you want to update
+router.post('/', (request, response, next) => {
+  const { firstName, lastName, password, email, isAdmin } = request.body;
+
+  pool.query(
+    'INSERT INTO users(firstName, lastName, password, email, isAdmin) VALUES($1, $2, $3, $4, $5)', 
+    [firstName, lastName, password, email, isAdmin],
+    (err, res) => {
+      if (err) return next(err);
+    }
+  );
 });
 
 module.exports = router;
