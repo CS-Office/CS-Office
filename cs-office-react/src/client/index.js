262bbf46774aea175b437a8366378ad2ecc9628b
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-03-15 14:58:31 -0700

    Merging partial rework of login

--- cs-office-react/src/client/index.js
+++
@@ -1,7 +0,0 @@
-// This is the entry point for our app
-// import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('root'));
