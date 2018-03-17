71342698e9c9bfbb76c0ac9a9c9c3f3a0b9d1dd3
Starvon Washington <staronejazz@yahoo.com>
2018-03-16 21:34:21 -0700

    database envirnoment knex

---
+++ csoffice_server_sql_2/server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+
+const PORT = 5555;
+const app = express();
+
+const users = require('./api/users');
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/api/users', users);
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    // render the error page if in production hide the stack trace
+    res.json({
+        message: err.message,
+        error: req.app.get('env') === 'development' ? err : {},
+    });
+});
+app.listen(PORT, () => {
+    console.log('Server started listening on port 5555');
+});
+
+module.exports = app;
