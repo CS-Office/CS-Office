d6e3b233dd4a0ba341d8408f4771c0bee907eedc
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-03-19 14:53:34 -0700

    User Button

--- csoffice_react/src/client/components/App.jsx
+++ csoffice_react/src/client/components/App.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import Video from './Video.jsx';
-
-import NavBar from './NavBar';
+import Video from './Video';
+import Header from './Header';
 import Office from './Office';
 import Login from './Login';
 import Signup from './Signup';
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     // MAKE SURE TO CHANGE BACK TO FALSE!!!!!!!!!!!!!
-    this.state = { 
-      isAuthenticated: false,
+    this.state = {
+      isAuth: false,
       isAdmin: true,
-      adminName: 'Admin' 
+      adminName: 'Admin',
+      user: {},
     };
     this.authenticate = this.authenticate.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   authenticate() {
-    this.setState({ ...this.state, isAuthenticated: true });
+    this.setState({ ...this.state, isAuth: true });
+  }
+
+  logout() {
+    this.setState({ ...this.state, isAuth: false });
   }
 
   render() {
+    const { isAuth } = this.state;
     const PrivateRoute = ({ component: Component, ...rest }) => (
-      <Route
-        {...rest}
-        render={() => (this.state.isAuthenticated ? <Component /> : <Redirect to="/login" />)}
-      />
+      <Route {...rest} render={() => (isAuth ? <Component /> : <Redirect to="/login" />)} />
     );
 
     return (
       <div className="wrapper">
-        <NavBar />
+        <Header isAuth={isAuth} logout={this.logout} />
         <main>
           <Switch>
             <Route exact path="/" render={() => <Redirect to="/login" />} />
@@ -42,11 +44,7 @@ class App extends React.Component {
             <Route
               path="/login"
               render={() =>
-                (this.state.isAuthenticated ? (
-                  <Redirect to="/office" />
-                ) : (
-                  <Login oAuthSuccess={this.authenticate} />
-                ))
+                (isAuth ? <Redirect to="/office" /> : <Login oAuthSuccess={this.authenticate} />)
               }
             />
             <PrivateRoute path="/office" component={Office} />
