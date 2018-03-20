8c4bd6854bfdd90499b7f36c503e0a94785fa8df
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-03-20 15:47:35 -0700

    Added the Code Editor

--- csoffice_react/src/client/components/Editor.jsx
+++ csoffice_react/src/client/components/Editor.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import Solution from './Solution';
+import React, { Component } from 'react';
+import { ButtonToolbar, Button } from 'react-bootstrap';
+// import { UnControlled as CodeMirror } from 'react-codemirror2';
+import CodeEditor from './../js/code_editor';
 
-const Editor = () => (
-  <div>
-    <Solution />
-    I am the Editor
-  </div>
-);
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      socket: this.props,
+    };
+  }
+
+  componentDidMount() {
+    CodeEditor(this.state.socket);
+  }
+
+  render() {
+    return (
+      <div className="editor-wrapper">
+        <div id="editor" />
+        <ButtonToolbar><Button id="run-code" bsStyle="primary">Run Code</Button></ButtonToolbar>
+        <ul id="solution" />
+      </div>
+    );
+  }
+}
 
 export default Editor;
